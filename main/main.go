446491fa23ef0d45f0fa4af1c583887041f0634e
sakastudio <sakastudio100@gmail.com>
2021-05-08 23:52:37 +0900

    for文

--- main/main.go
+++ main/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a,b := 1000,100
-	if a>b{
-		fmt.Println("aのほうがデカい")
-	}else if a==b{
-		fmt.Println("aとb同じ")
-	}else {
-		fmt.Println("bのほうがデカい")
+	for i :=0;i<10;i++{
+		fmt.Println(i)
 	}
 }
